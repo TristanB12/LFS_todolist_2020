852e0c029a919657a0615c0f2a860aca04059e1e
TristanB12 <tristan.bourgeois@epitech.eu>
2020-11-03 19:31:09 +0400

    post work correctly

--- src/components/postItem.vue
+++ src/components/postItem.vue
@@ -2,18 +2,21 @@
     <div class="post-item">
         <postUser :postUser="postItem.post_user"></postUser>
         <postContent :postContent="postItem.post_content"></postContent>
+        <postStats :postStats="postItem.stats"></postStats>
     </div>
 </template>
 
 <script>
 import postUser from '@/components/postUser.vue';
 import postContent from '@/components/postContent.vue';
+import postStats from '@/components/postStats.vue';
 
     export default {
         name: 'postItem',
         components: {
             postUser,
             postContent,
+            postStats,
         },
         props: {
             postItem: {
@@ -32,5 +35,6 @@ import postContent from '@/components/postContent.vue';
     margin: 10px auto 10px auto;
     border-radius: 15px;
     background-color: #1e272e;
+    overflow: hidden;
 }
 </style>
---
+++ src/components/postStats.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="post-stats">
+        <div class="likes">
+            <img src="../assets/like_before.png" alt="like button" class="like-button">
+            <h4>{{ postStats.likes }}</h4>
+        </div>
+        <div class="shares">
+            <img src="../assets/share_before.png" alt="like button" class="share-button">
+            <h4>{{ postStats.shares }}</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'postStats',
+        props: {
+            postStats: {
+                type: Object,
+                default: () => {},
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+.post-stats {
+    display: flex;
+    justify-content: space-around;
+    background-color: #222f3e;
+}
+
+.post-stats div {
+    display: flex;
+}
+
+.post-stats div * {
+    margin: 10px;
+}
+
+img {
+    width: 50px;
+    height: 50px;
+}
+
+h4 {
+    font-size: 22px;
+    color: #576574;
+}
+
+</style>
